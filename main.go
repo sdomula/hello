8364d23f22a2c612a20c690223e9436a3df6d839
Stan Domula <stan.domula@lovoo.com>
2018-01-18 16:53:38 +0100

    inital commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", hello)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusOK)
+	fmt.Fprintf(res, "Hello World")
+}
