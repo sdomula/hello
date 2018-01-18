68dcf65a68a3438a7392396d5d7080281fa96831
Stan Domula <stan.domula@lovoo.com>
2018-01-18 16:58:31 +0100

    add deployment.yaml

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/healthz", func(res http.ResponseWriter, _ *http.Request) {
+		res.WriteHeader(http.StatusOK)
+	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusOK)
-	fmt.Fprintf(res, "Hello Docker")
+	fmt.Fprintf(res, "Hello Kubernetes")
 }
